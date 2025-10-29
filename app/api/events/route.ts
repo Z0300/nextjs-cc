79a2067b0d8ad6eeb77317e6c65be35a44a679d0
subZero <marvinfordan08@gmail.com>
2025-10-29 11:28:59 +0800

    Implement api routes

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import connectDB from "@/lib/mongodb";
 import Event from '@/database/event.model'
+import {v2 as cloudinary} from 'cloudinary'
 
 export async function POST(req: NextRequest) {
     try {
@@ -16,6 +17,23 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({message: 'Invalid json body format.'}, {status: 400})
         }
 
+        const file = formData.get('image') as File;
+
+        if (!file)
+            return NextResponse.json({message: 'Image file is required.'}, {status: 400});
+
+        const arrBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({resource_type: 'image', folder: 'DevEvents'}, (error, result) => {
+                if (error) return reject(error);
+                resolve(result)
+            }).end(buffer)
+        })
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({message: 'Event created successfully.', event: createdEvent}, {status: 201});
@@ -29,3 +47,16 @@ export async function POST(req: NextRequest) {
 
     }
 }
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({createdAt: -1});
+
+        return NextResponse.json({message: 'Event list successfully.', events}, {status: 200});
+    } catch (e) {
+        return NextResponse.json({message: 'Event fetching failed', error: e}, {status: 500})
+    }
+}
+
---
+++ app/events/[slug]/page.tsx
@@ -0,0 +1,103 @@
+import {notFound} from "next/navigation";
+import Image from "next/image";
+import EventDetailItem from "@/components/EventDetails";
+import BookEvent from "@/components/BookEvent";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+const EventAgenda = ({agendaItems}: { agendaItems: string[]; }) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((agenda) => (
+                <li key={agenda}>{agenda}</li>
+            ))}
+        </ul>
+    </div>
+)
+
+const EventTags = ({tags}: { tags: string[]; }) => (
+    <div className="flex flex-row gap-1.5 flex-wrap">
+
+        {tags.map((tag) => (
+            <div className="pill" key={tag}>{tag}</div>
+        ))}
+
+    </div>
+)
+
+const EventDetailsPage = async ({params}: { params: Promise<{ slug: string }> }) => {
+    const {slug} = await params;
+    const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+    const {
+        data: {
+            description,
+            image,
+            overview,
+            date,
+            time,
+            location,
+            mode,
+            agenda,
+            audience,
+            organizer,
+            tags
+        }
+    } = await request.json();
+
+    if (!description) return notFound();
+
+    const bookings = 10;
+
+
+    return (
+        <section id="event">
+            <div className="header">
+                <h1>Event Description</h1>
+                <p>{description}</p>
+            </div>
+            <div className="details">
+                {/* Left Side - Event Content */}
+                <div className="content">
+                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+                    <section className="flex-col-gap-2">
+                        <h2>Event Details</h2>
+                        <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date}/>
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time}/>
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location}/>
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode}/>
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience}/>
+                    </section>
+                    <EventAgenda agendaItems={JSON.parse(agenda[0])}/>
+
+                    <section className="flex-col-gap-2">
+                        <h2>About Organizer</h2>
+                        <p>{organizer}</p>
+                    </section>
+
+                    <EventTags tags={JSON.parse(tags[0])}/>
+                </div>
+                {/* Right Side - Booking Form */}
+                <aside className="booking">
+                    <div className="signup-card">
+                        <h2>Book your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className="text-sm">
+                                Join {bookings} people who have already booked their spot!
+                            </p>
+                        ) : (
+                            <p className="text-sm">Be the first to book your spot!</p>
+                        )}
+
+                        <BookEvent />
+                    </div>
+                </aside>
+            </div>
+        </section>
+    )
+}
+export default EventDetailsPage
