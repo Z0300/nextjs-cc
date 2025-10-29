24492c6df20df019486de9d75a2111436e846a0f
subZero <marvinfordan08@gmail.com>
2025-10-29 15:59:50 +0800

    fix merge conflict

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,74 +1,14 @@
 import {Suspense} from "react";
 import EventDetails from "@/components/EventDetails";
 
-const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
-
+const EventDetailsPage = async ({params}: { params: Promise<{ slug: string }> }) => {
 
     return (
-<<<<<<< Updated upstream
-        <section id="event">
-            <div className="header">
-                <h1>Event Description</h1>
-                <p>{description}</p>
-            </div>
-            <div className="details">
-                {/* Left Side - Event Content */}
-                <div className="content">
-                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
-                    <section className="flex-col-gap-2">
-                        <h2>Overview</h2>
-                        <p>{overview}</p>
-                    </section>
-                    <section className="flex-col-gap-2">
-                        <h2>Event Details</h2>
-                        <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date}/>
-                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time}/>
-                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location}/>
-                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode}/>
-                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience}/>
-                    </section>
-                    <EventAgenda agendaItems={agenda}/>
-
-                    <section className="flex-col-gap-2">
-                        <h2>About Organizer</h2>
-                        <p>{organizer}</p>
-                    </section>
-
-                    <EventTags tags={tags}/>
-                </div>
-                {/* Right Side - Booking Form */}
-                <aside className="booking">
-                    <div className="signup-card">
-                        <h2>Book your Spot</h2>
-                        {bookings > 0 ? (
-                            <p className="text-sm">
-                                Join {bookings} people who have already booked their spot!
-                            </p>
-                        ) : (
-                            <p className="text-sm">Be the first to book your spot!</p>
-                        )}
-
-                        <BookEvent/>
-                    </div>
-                </aside>
-            </div>
-
-            <div className="flex w-full flex-col gap-4 pt-20">
-                <h2>Similar Events</h2>
-                <div className="events">
-                    {similarEvents.length > 0 && similarEvents.map((similarEvent: IEvent) => (
-                        <EventCard key={similarEvent._id.toString()} {...similarEvent} />
-                    ))}
-                </div>
-            </div>
-        </section>
-=======
         <main>
             <Suspense fallback={<div>Loading...</div>}>
-                <EventDetails params={params} />
+                <EventDetails params={params}/>
             </Suspense>
         </main>
->>>>>>> Stashed changes
     )
 }
 export default EventDetailsPage
